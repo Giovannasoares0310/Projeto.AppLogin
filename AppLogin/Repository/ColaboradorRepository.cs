b50420199d2a53a28d5d5043d3acd5c1763dcb8b
LauraX07 <lauxavi1357@gmail.com>
2026-05-22 09:30:56 -0300

    Adicionando interface e class do Colaborador

---
+++ AppLogin/Repository/ColaboradorRepository.cs
@@ -0,0 +1,60 @@
+﻿using AppLogin.Models;
+using AppLogin.Repository.Contract;
+using X.PagedList;
+
+namespace AppLogin.Repository
+{
+    public class ColaboradorRepository : IColaboradorRepository
+    {
+        private readonly string _conexaoMySQL;
+
+        public ColaboradorRepository(IConfiguration conf)
+        {
+            _conexaoMySQL = conf.GetConnectionString("ConexãoMySQL");
+        }
+        public void Atualizar(Colaborador colaborador)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AtualizarSenha(Colaborador colaborador)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(Colaborador colaborador)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Colaborador Login(string Email, string Senha)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Colaborador ObterColaborador(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Colaborador> ObterColaboradorPorEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Colaborador> ObterTodosColaboradores()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPagedList<Colaborador> ObterTodosColaboradores(int? pagina)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- AppLogin/obj/Debug/net10.0/AppLogin.AssemblyInfo.cs
+++ AppLogin/obj/Debug/net10.0/AppLogin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppLogin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21da31429e47f959a7d9a623cb0bca19a4803167")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+488e9180d27c7fec7f015b29c2b1f344e6635329")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppLogin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppLogin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
