f8304d3910abcb4a73a77f3089d32b52c191daaa
giovannasoares0310 <giovannasoares031008@gmail.com>
2026-05-26 09:21:36 -0300

    Adicionando métodos de Obter Todos Colaboradores

--- AppLogin/Repository/ColaboradorRepository.cs
+++ AppLogin/Repository/ColaboradorRepository.cs
@@ -5,20 +5,39 @@ using MySql.Data.MySqlClient;
 using MySqlX.XDevAPI;
 using System.Data;
 using X.PagedList;
+using X.PagedList.Extensions;
 
 namespace AppLogin.Repository
 {
     public class ColaboradorRepository : IColaboradorRepository
     {
         private readonly string _conexaoMySQL;
-
+        private IConfiguration _conf;
         public ColaboradorRepository(IConfiguration conf)
         {
             _conexaoMySQL = conf.GetConnectionString("ConexãoMySQL");
+            _conf = conf;
         }
         public void Atualizar(Colaborador colaborador)
         {
-            throw new NotImplementedException();
+            string Tipo = ColaboradorTipoConstant.Comum;
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("upadate Colaborador set Nome=@Nome, Email=@Email," +
+                    " Senha=@Senha, Tipo=@Tipo where Id=@Id)", conexao);
+                {
+                    cmd.Parameters.Add("@Id", MySqlDbType.VarChar).Value = colaborador.Id;
+                    cmd.Parameters.Add("@Nome", MySqlDbType.VarChar).Value = colaborador.Nome;
+                    cmd.Parameters.Add("@Email", MySqlDbType.VarChar).Value = colaborador.Email;
+                    cmd.Parameters.Add("@Senha", MySqlDbType.VarChar).Value = colaborador.Senha;
+                    cmd.Parameters.Add("@Tipo", MySqlDbType.VarChar).Value = Tipo;
+
+                    cmd.ExecuteNonQuery();
+                    conexao.Close();
+                }
+            }
         }
 
         public void AtualizarSenha(Colaborador colaborador)
@@ -86,7 +105,28 @@ namespace AppLogin.Repository
         }
         public Colaborador ObterColaborador(int Id)
         {
-            throw new NotImplementedException();
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from colaborador where Id=@Id", conexao);
+                cmd.Parameters.AddWithValue("@Id", Id);
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                MySqlDataReader dr;
+
+                Colaborador colaborador = new Colaborador();
+                dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                while (dr.Read())
+                {
+
+                    colaborador.Id = Convert.ToInt32(dr["id"]);
+                    colaborador.Nome = (string)(dr["Nome"]);
+                    colaborador.Email = (string)(dr["Email"]);
+                    colaborador.Senha = (string)(dr["Senha"]);
+                    colaborador.Tipo = (string)(dr["Tipo"]);
+                }
+                return colaborador;
+            }
         }
 
         public List<Colaborador> ObterColaboradorPorEmail(string email)
@@ -96,12 +136,67 @@ namespace AppLogin.Repository
 
         public IEnumerable<Colaborador> ObterTodosColaboradores()
         {
-            throw new NotImplementedException();
+            List<Colaborador> colabList = new List<Colaborador>();
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from colaborador", conexao);
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+
+                DataTable dt = new DataTable();
+
+                da.Fill(dt);
+                conexao.Close();
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    colabList.Add(
+                        new Colaborador
+                        {
+                            Id = Convert.ToInt32(dr["id"]),
+                            Nome = (string)(dr["Nome"]),
+                            Email = (string)(dr["Email"]),
+                            Senha = (string)(dr["Senha"]),
+                            Tipo = (string)(dr["Tipo"])
+                        });
+                }
+                return colabList;
+            }
         }
 
         public IPagedList<Colaborador> ObterTodosColaboradores(int? pagina)
         {
-            throw new NotImplementedException();
+            int RegistroPorPagina = _conf.GetValue<int>("RegistroPorPagina");
+
+            int NumeroPagina = pagina ?? 1;
+            List<Colaborador> ListCat = new List<Colaborador>();
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from colaborador;", conexao);
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+
+                da.Fill(dt);
+                conexao.Close();
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    ListCat.Add(
+                        new Colaborador
+                        {
+                            Id = Convert.ToInt32(dr["Id"]),
+                            Nome = (string)(dr["Nome"]),
+                            Senha = (string)(dr["Senha"]),
+                            Email = (string)(dr["Email"]),
+                            Tipo = (string)(dr["Senha"])
+
+                        });
+                }
+                return ListCat.ToPagedList<Colaborador>(NumeroPagina, RegistroPorPagina);
+            }
         }
     }
 }
--- AppLogin/obj/Debug/net10.0/AppLogin.AssemblyInfo.cs
+++ AppLogin/obj/Debug/net10.0/AppLogin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppLogin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+488e9180d27c7fec7f015b29c2b1f344e6635329")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd57e34fc311ded2ee9425cd108f355ce67830a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppLogin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppLogin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
